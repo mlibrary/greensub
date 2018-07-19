651e033aaa34cefcca710dac41fc8262711916ed
Jeremy Morse <jgmorse@umich.edu>
2018-07-19 17:52:34 -0400

    Basic Product class and tests.

--- lib/product.rb
+++ lib/product.rb
@@ -3,6 +3,7 @@
 require 'yaml'
 
 class Product
+<<<<<<< 4b7b754eedd9eb31bf377e00adbd5f7b4eb4b951
   attr_accessor :id, :external_id, :name, :host, :components
 
   def initialize(id)
@@ -11,6 +12,15 @@ class Product
     @external_id = data["#{@id}"]["external_id"]
     @name = data["#{@id}"]["name"]
     @host = data["#{@id}"]["host"]
+=======
+  attr_accessor :id, :external_id, :name, :components
+
+  def initialize(id)
+    @id = id
+    data = YAML.load_file('config/products.yaml')
+    @external_id = data["#{@id}"]["external_id"]
+    @name = data["#{@id}"]["name"]
+>>>>>>> Basic Product class and tests.
   end
 
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,9 +1,13 @@
+<<<<<<< 4b7b754eedd9eb31bf377e00adbd5f7b4eb4b951
 # frozen_string_literal: true
 
+=======
+>>>>>>> Basic Product class and tests.
 require 'Product'
 
 RSpec.describe Product do
   describe "Example Product" do
+<<<<<<< 4b7b754eedd9eb31bf377e00adbd5f7b4eb4b951
     test = Product.new('test')
     it "has an external id" do
       expect(test.external_id).to eq("foo")
@@ -13,6 +17,14 @@ RSpec.describe Product do
     end
     it "has a host" do
       expect(test.host).to eq("example")
+=======
+    heb = Product.new('heb')
+    it "has an external id" do
+      expect(heb.external_id).to eq("heb")
+    end
+    it "has a name" do
+      expect(heb.name).to eq("ACLS Humanities E-Book")
+>>>>>>> Basic Product class and tests.
     end
   end
 end
