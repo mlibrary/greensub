6a38174a22014a48c3611d88bebc0ac9bd480e12
Jeremy Morse <jgmorse@umich.edu>
2018-07-20 10:11:05 -0400

    Added Product.host

--- lib/product.rb
+++ lib/product.rb
@@ -3,6 +3,7 @@
 require 'yaml'
 
 class Product
+<<<<<<< eb17d18cc0993c795641a1d97e61bf7bc8eff715
 <<<<<<< 4b7b754eedd9eb31bf377e00adbd5f7b4eb4b951
   attr_accessor :id, :external_id, :name, :host, :components
 
@@ -14,13 +15,20 @@ class Product
     @host = data["#{@id}"]["host"]
 =======
   attr_accessor :id, :external_id, :name, :components
+=======
+  attr_accessor :id, :external_id, :name, :host, :components
+>>>>>>> Added Product.host
 
   def initialize(id)
     @id = id
     data = YAML.load_file('data/products.yaml')
     @external_id = data["#{@id}"]["external_id"]
     @name = data["#{@id}"]["name"]
+<<<<<<< eb17d18cc0993c795641a1d97e61bf7bc8eff715
 >>>>>>> Basic Product class and tests.
+=======
+    @host = data["#{@id}"]["host"]
+>>>>>>> Added Product.host
   end
 
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,12 +1,18 @@
+<<<<<<< eb17d18cc0993c795641a1d97e61bf7bc8eff715
 <<<<<<< 4b7b754eedd9eb31bf377e00adbd5f7b4eb4b951
 # frozen_string_literal: true
 
 =======
 >>>>>>> Basic Product class and tests.
+=======
+# frozen_string_literal: true
+
+>>>>>>> Added Product.host
 require 'Product'
 
 RSpec.describe Product do
   describe "Example Product" do
+<<<<<<< eb17d18cc0993c795641a1d97e61bf7bc8eff715
 <<<<<<< 4b7b754eedd9eb31bf377e00adbd5f7b4eb4b951
     test = Product.new('test')
     it "has an external id" do
@@ -19,12 +25,22 @@ RSpec.describe Product do
       expect(test.host).to eq("example")
 =======
     heb = Product.new('heb')
+=======
+    test = Product.new('test')
+>>>>>>> Added Product.host
     it "has an external id" do
-      expect(heb.external_id).to eq("heb")
+      expect(test.external_id).to eq("foo")
     end
     it "has a name" do
+<<<<<<< eb17d18cc0993c795641a1d97e61bf7bc8eff715
       expect(heb.name).to eq("ACLS Humanities E-Book")
 >>>>>>> Basic Product class and tests.
+=======
+      expect(test.name).to eq("Foo Means Fake")
+    end
+    it "has a host" do
+      expect(test.host).to eq("example")
+>>>>>>> Added Product.host
     end
   end
 end
