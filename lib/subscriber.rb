4c8af42dd5a2f01f8b266df867efb63b14ac5e31
Jeremy Morse <jgmorse@umich.edu>
2018-07-21 18:18:22 -0400

    Refactor

--- lib/subscriber.rb
+++ lib/subscriber.rb
@@ -2,19 +2,13 @@
 
 class Subscriber
   attr_accessor :id, :external_id
+
+  def initialize(id)
+    @id = id
+    @external_id = @id #unless we have a record that says otherwise....
+  end
 end
 
 class Institution < Subscriber
   attr_accessor :name
-
-  def initialize(id)
-    @id=id
-    @external_id = @id #unless we have a record that says otherwise....
-    get_keycard
-  end
-
-  def get_keycard
-    #stub
-  end
-
 end
