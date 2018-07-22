6563a9c8ccaaf1c59de3c7bce6a31b6d8d56e45b
Jeremy Morse <jgmorse@umich.edu>
2018-07-22 12:49:28 -0400

    refactoting and using case for switching on authz/expire action

--- bin/authz_inst.rb
+++ bin/authz_inst.rb
@@ -1,37 +1,39 @@
 # frozen_string_literal: true
 
 require_relative '../lib/product'
+require_relative '../lib/subscriber'
+require_relative '../lib/lease'
 
 #Get params: inst_id, product_id, action
-inst_id = '111'
-product_id = 'yoyo'
+inst_id = '999'
+product_id = 'heb'
 action = :authz
 $TESTING = true
 
 
-prod = Product.new(product_id)
-unless prod.hosted?
-  puts " Product #{prod} does not have a host, quitting...."
+product = Product.new(product_id)
+unless product.hosted?
+  puts " Product #{product} does not have a host, quitting...."
   exit!(0)
 end
 
-#prod.host.lessees
-
 inst = Institution.new(inst_id)
-
-if ! prod.host.knows_subscriber(inst)
-    if action == :expire
-      puts "Institution is not on the host, so nothing to expire"
-      exit
-    elsif action == :authz
-        #create Inst on Host
-        unless prod.host.add_subscriber(inst)
-          abort "Can't add subscriber at host"
-        end
-    end
-end
+lease = Lease.new(product_id, inst_id)
 
 #if action is to expire
+case action
+when :authz
+  if ! product.host.knows_subscriber(inst)
+    unless product.host.add_subscriber(inst)
+      abort "Can't add subscriber at host"
+    end
+  end
+when :expire
+  if ! product.host.knows_subscriber(inst)
+    puts "Institution is not on the host, so nothing to expire"
+    exit
+  end
+end
     #de-authz the inst to this product at the host
   #else
     #authz the inst at the host
