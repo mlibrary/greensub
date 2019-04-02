fbd9b0a7dae8593aec32a232406e78a85a9ed923
Jeremy Morse <jgmorse@umich.edu>
2019-04-02 15:20:36 -0400

    Remove all uses of :handle keyword for Components as it's no longer supported by turnsole.

--- lib/component.rb
+++ lib/component.rb
@@ -1,21 +1,16 @@
 # frozen_string_literal: true
 
 class Component
-  attr_accessor :hosted_id, :sales_id, :name, :handle, :open_access, :product
+  attr_accessor :hosted_id, :sales_id, :name, :open_access, :product
 
   def initialize(ext_id, s_id, prod_obj, name = nil)
     @hosted_id = ext_id # NOID
     @sales_id = s_id || nil
     @name = name || nil
-    @handle = get_handle(@hosted_id)
     @product = prod_obj
   end
 
   def hosted?
     @product.host.knows_component?(self)
-  end
-
-  def get_handle(hosted_id)
-    "2027/fulcrum.#{hosted_id}"
   end
 end
